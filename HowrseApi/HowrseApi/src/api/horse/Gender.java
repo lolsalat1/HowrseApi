ededde98a1ba355b52aff6eb4b27650e174fd5b5
Lorenz <Lorenz@134.96.49.42>
2018-11-28 19:47:33 +0100

    Every Class needs some comments

--- HowrseApi/HowrseApi/src/api/horse/Gender.java
+++ HowrseApi/HowrseApi/src/api/horse/Gender.java
@@ -1,9 +1,23 @@
 package api.horse;
 
+/**
+ * There are 3 Genders - WAIT WHAT??!
+ */
 public enum Gender {
 
+	/**
+	 * Male
+	 */
 	MALE,
+	
+	/**
+	 * Female
+	 */
 	FEMALE,
+	
+	/**
+	 * Uhmm, well ... I don't really know 
+	 */
 	UNKNOWN
 	
 }
