2c82ba6d02b6b4c2b514f91cdc73086a295aab5e
Lorenz <Lorenz@AsusNB>
2018-11-30 23:42:53 +0100

    we need to add everything

--- HowrseApi/HowrseApi/src/api/request/requests/SearchHorseRequest.java
+++ HowrseApi/HowrseApi/src/api/request/requests/SearchHorseRequest.java
@@ -17,6 +17,37 @@ import api.request.RequestHandler;
  */
 public class SearchHorseRequest {
 
+	public static SearchHorseResponse sendRequest(SORT sort, FILTER filter, API api) throws ApiException {
+		try {
+			
+			List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+			parameters.add(new BasicNameValuePair("go", "1"));
+			parameters.add(new BasicNameValuePair("filter", (filter == FILTER.specialisation_classique ? "specialisation-classique" : (filter == FILTER.specialisation_western ? "specialisation-western" : filter.name()))));
+			parameters.add(new BasicNameValuePair("sort", (sort == SORT.nonne ? "" : sort.name())));
+			String response = RequestHandler.read(api.requests.ApiRequest("elevage/chevaux/searchHorse", parameters, api));
+			
+			String[] split = response.split("href=\"/elevage/chevaux/cheval");
+			
+			SearchHorseResponse res = new SearchHorseResponse();
+			
+			HashMap<Integer, Horse> horses = new HashMap<Integer, Horse>(split.length-1);
+			
+			for(int i = 1; i < split.length; i++) {
+				String split2 = split[i].split("=")[1].split("<")[0]; // I might find a more efficient way
+				int id = Integer.parseInt(split2.split("\"")[0]);
+				horses.put(id, new Horse(split2.split(">")[1], id));
+				
+			}
+			res.horses = horses;
+			res.sucess = !horses.isEmpty();
+			
+			return res;
+			
+		} catch(Exception e) {
+			throw new ApiException(e, "SearchHorse Request failed");
+		}
+	}
+	
 	public static SearchHorseResponse sendRequestSimple(SORT sort, FILTER filter, API api) throws ApiException{
 		try {
 			
@@ -107,6 +138,17 @@ public class SearchHorseRequest {
 		nogestation,
 		male,
 		female
+	}
+	
+	public static class Comparator {
+		public String name;
+		public int value;
+		public COMPARE compare;
+	}
+	
+	public static enum COMPARE {
+		g,
+		l
 	}
 	
 	public static class SearchHorseResponse {
